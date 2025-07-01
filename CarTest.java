89a177ffdda109db43a90227f91867c3cb7ea55f
lizy-10 <lizya7133@gmail.com>
2025-07-01 10:11:14 +0530

    this object

---
+++ CarTest.java
@@ -0,0 +1,25 @@
+package one;
+
+class Car {
+	 String model;
+	 String color;
+	 Car(String model, String color) {
+	 this.model = model;
+	 this.color = color;
+	 }
+	 void start() {
+	 System.out.println(model + " is starting.");
+	 }
+	 void stop() {
+	 System.out.println(model + " is stopping.");
+	 }
+	}
+	public class CarTest {
+	 public static void main(String[] args) {
+	 Car myCar = new Car("Toyota Corolla", "Red");
+	 System.out.println("Car Model: " + myCar.model);
+	 System.out.println("Car Color: " + myCar.color);
+	 myCar.start(); 
+	 myCar.stop();
+	 }
+	} 
